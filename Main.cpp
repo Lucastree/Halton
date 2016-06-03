419092a9184f22485c4cbe8a1a5c9909fa01243b
Lucastree <lebron1313@163.com>
2016-06-04 00:54:51 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,24 @@
 #include<bits/stdc++.h>
-double halton (int index, int base);
-int main(int ac, char** av)
-{
-    int base = 2; 
-    printf("| halton seq |\n");
-    printf("|---: | :--------: |\n");
-    for (int index = 1; index <= 20; ++index ) {
-        printf("| %3d | %10.8f |\n", index, halton( index, base ) );
-    }
-    return 0;
-}
 double halton (int index, int base)
 {
     double frac = 1.0 / (double)base;
     double result = 0.0;
-    while ( index > 0 ) {
+    while ( index > 0 ) 
+    {
         result += frac * ( index % base );
         index = (int)( (double)index / (double)base );
         frac /= (double)base;
     }
     return result;
+}
+int main(int ac, char** av)
+{
+    int base = 2; 
+    printf("| halton seq |\n");
+    printf("|---: | :--------: |\n");
+    for (int index = 1; index <= 20; ++index ) 
+    {
+        printf("| %3d | %10.8f |\n", index, halton( index, base ) );
+    }
+    return 0;
 }
